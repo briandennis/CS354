f900629c7c6c7aedb790cf8f4289e352bb14bd5a
Brian Dennis <briandennis321@gmail.com>
2016-01-22 16:05:19 -0600

    added p0

---
+++ p0/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int myage = 19;
+  char myname[] = "Brian";
+  char myalpha = 'T';  
+
+  printf("hello, world\n");
+  printf("My name is %s\n", myname);
+  printf("My age is %d\n", myage);
+  printf("My favourite alphabet is %c\n", myalpha);
+  return 0;
+}
