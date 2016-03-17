5b2a4a846a6670e8aa12fbe0d8545f38e0b70dcf
Brian Dennis <briandennis321@gmail.com>
2016-03-17 09:36:18 -0500

    added struct and pointer arrow usage

--- mallocForDayz/main.c
+++ mallocForDayz/main.c
@@ -4,29 +4,20 @@
 int main (){
 
   struct Animal {
-    char name[10];
+    char species[10];
     int age;
     double attackMult;
     int health;
   };
 
-    *Animal createAnimal (char name[10], int age, double attackMult){
-
-    struct Animal returnAnimal = (struct Animal) malloc(sizeof(struct Animal));
-
-    strcpy(&returnAnimal.name, name);
-    returnAnimal.name = name;
-    returnAnimal.age = age;
-    returnAnimal.attackMult = attackMult;
-    returnAnimal.health = 100;
-
-    return &returnAnimal;
-  }
-
-  struct Animal *bobby = createAnimal("Giraffe",20,1.9);
+  struct Animal bobby = {"Giraffe", 20, 1.9, 100};
 
 
-  printf("Bobby's name: %s", *bobby.name);
+  printf("Bobby's species: %s \n", bobby.species);
+
+  struct Animal * bobbyPointer = &bobby;
+
+  printf("Bobby's species via pointer: %s", bobbyPointer -> species);
 
   return 1;
 
