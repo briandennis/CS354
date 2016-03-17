1b0c5515125f9e83a6d183e88ce6fa2f18927392
Brian Dennis <briandennis321@gmail.com>
2016-03-17 10:23:13 -0500

    fxed placement, added health function

--- mallocForDayz/main.c
+++ mallocForDayz/main.c
@@ -2,17 +2,28 @@
 #import <string.h>
 
 #define PI 3.141592
+#define TRUE 1
+#define FALSE 0
+
+struct Animal {
+  char species[10];
+  int age;
+  double attackMult;
+  int health;
+  int alive;
+};
+
+void decreaseHealth (struct Animal * animal, int decrement){
+  animal -> health = (animal -> health) - decrement;
+  if(animal -> health < 0){
+    animal -> alive = FALSE;
+    printf("A %s has died :\( \n", animal -> species);
+  }
+}
 
 int main (){
 
-  struct Animal {
-    char species[10];
-    int age;
-    double attackMult;
-    int health;
-  };
-
-  struct Animal bobby = {"Giraffe", 20, 1.9, 100};
+  struct Animal bobby = {"Giraffe", 20, 1.9, 100, TRUE};
 
 
   printf("Bobby's species: %s \n", bobby.species);
@@ -22,6 +33,8 @@ int main (){
   printf("Bobby's species via pointer: %s \n", bobbyPointer -> species);
 
   printf("Bobby's attack value for throwing a pie is: %f \n", PI * bobby.attackMult);
+
+  decreaseHealth(&bobby, 110);
 
   return 1;
 
